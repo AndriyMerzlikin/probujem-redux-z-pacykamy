808f0fd62caaf02d1851dfff7d09a7715229bc60
AndriyMerzlikin <lastheroofme@gmail.com>
2024-03-02 09:13:25 +0200

    sfgh

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,14 @@
 import { useSelector } from "react-redux";
 import { Account } from "./Account";
 import { LangSwitcher } from "./LangSwitcher";
+import BasicDateCalendar from "./ProductsPage/Products";
 
 function App() {
   const lang = useSelector((state) => state.locale.lang);
 
   return (
     <div className="App">
+      <BasicDateCalendar />
       <LangSwitcher />
       <Account />
       <p>
